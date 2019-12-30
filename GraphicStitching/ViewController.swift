4ff43459e7fbc8fb871265384c619a66a0cca196
Ge Will <531sunlight@gmail.com>
2019-12-30 15:10:54 +0800

    Initial Commit

---
+++ GraphicStitching/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GraphicStitching
+//
+//  Created by Will on 30/12/2019.
+//  Copyright © 2019 Ge Will. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
